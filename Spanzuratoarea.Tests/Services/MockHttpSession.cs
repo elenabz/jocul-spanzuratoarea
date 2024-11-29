59e2a9250f2d27908549bb46d7414d0ccfb766ad
elena_bb <elena.m.bizoi@gmail.com>
2024-11-29 19:01:56 +0200

    add mock class for ISession

---
+++ Spanzuratoarea.Tests/Services/MockHttpSession.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Spanzuratoarea.Tests.Services
+{
+    public class MockHttpSession : ISession
+    {
+        readonly Dictionary<string, object> _sessionStorage = [];
+        public bool IsAvailable => throw new NotImplementedException();
+
+        public string Id => throw new NotImplementedException();
+
+        IEnumerable<string> ISession.Keys => _sessionStorage.Keys;
+
+        public void Clear()
+        {
+            _sessionStorage.Clear();
+        }
+
+        public Task CommitAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task LoadAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(string key)
+        {
+            _sessionStorage.Remove(key);
+        }
+
+        public void Set(string key, byte[] value)
+        {
+            _sessionStorage[key] = Encoding.UTF8.GetString(value);
+        }
+
+        public bool TryGetValue(string key, [NotNullWhen(true)] out byte[]? value)
+        {
+            if (_sessionStorage[key] != null)
+            {
+                value = Encoding.ASCII.GetBytes(_sessionStorage[key].ToString());
+                return true;
+            }
+            value = null;
+            return false;
+        }
+
+        //public static void SetInt32(this ISession session, string key, int value)
+        //{
+        //    var bytes = new byte[]
+        //    {
+        //        (byte)(value >> 24),
+        //        (byte)(0xFF & (value >> 16)),
+        //        (byte)(0xFF & (value >> 8)),
+        //        (byte)(0xFF & value)
+        //    };
+        //    session.Set(key, bytes);
+        //}
+
+        //public static int? GetInt32(this ISession session, string key)
+        //{
+        //    var data = session.Get(key);
+        //    if (data == null || data.Length < 4)
+        //    {
+        //        return null;
+        //    }
+        //    return data[0] << 24 | data[1] << 16 | data[2] << 8 | data[3];
+        //}
+    }
+}
--- Spanzuratoarea.Tests/Services/SessionManagerTests.cs
+++ Spanzuratoarea.Tests/Services/SessionManagerTests.cs
@@ -1,6 +1,57 @@
-﻿namespace Spanzuratoarea.Tests.Services
+﻿using JoculSpanzuratoarea.Services;
+using Microsoft.AspNetCore.Http;
+using Moq;
+
+namespace Spanzuratoarea.Tests.Services
 {
     public class SessionManagerTests
     {
+        private readonly SessionManager _sessionManager;
+        private readonly MockHttpSession _httpSession;
+
+        public SessionManagerTests()
+        {
+            var httpContextAccessor = new Mock<IHttpContextAccessor>();
+            var httpContext = new Mock<HttpContext>();
+            _httpSession = new MockHttpSession();
+            httpContextAccessor.Setup(s => s.HttpContext).Returns(httpContext.Object);
+            httpContext.Setup(s => s.Session).Returns(_httpSession);
+            _sessionManager = new SessionManager(httpContextAccessor.Object);
+        }
+
+        [Fact]
+        public void DeleteSessionItems()
+        {
+            _sessionManager.DeleteSessionItems();
+            var result = _sessionManager.GetWord();
+            Assert.Equal("", result);
+        }
+
+        [Fact]
+        public void SetWord_GivenString()
+        {
+            string word = "spanzuratoarea";
+            _sessionManager.SetWord(word);
+            var result = _sessionManager.GetWord();
+            Assert.Equal(word, result);
+        }
+
+        [Fact]
+        public void SetGuessedFullWord()
+        {
+            int guessed = 1;
+            _sessionManager.SetGuessedFullWord();
+            var result = _sessionManager.GetGuessedFullWord();
+            Assert.Equal(guessed, result);
+        }
+
+        [Fact]
+        public void IncrementFailCount()
+        {
+            int count = 0;
+            _sessionManager.IncrementFailCount();
+            var result = _sessionManager.GetFailCount();
+            Assert.Equal(count, result);
+        }
     }
 }
